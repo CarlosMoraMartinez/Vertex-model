0ec91fa1cd48c9b1d117ebc9f1003c06cdb88b96
Mora Martinez Carlos <carmoma@dx5-biotek1.biocenter.helsinki.fi>
2019-11-05 11:44:54 +0200

    used git rm

--- call_plotOps.sh
+++
@@ -1,9 +0,0 @@
-
-
-fname=$1  #"190726_test"
-for i in {0..1000};
-do
- python plotOps.py $fname$i
- echo "$i "
-
-done
--- compile_gx.sh
+++
@@ -1,6 +0,0 @@
-
-
-g++ -std=c++11 -c GeneExpression.cpp
-g++ -std=c++11 -c test_gx.cpp
-g++ -o gx_test test_gx.o GeneExpression.o
-./gx_test 
