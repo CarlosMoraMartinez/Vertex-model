7c857b562265ef67098d6452eec69392b3cef56f
carmoma9 <carmoma9@gmail.com>
2020-06-09 17:13:40 +0200

    fixed plotEnsembleLandmarks.R

--- scripts/plotEnsembleLandmarks.R
+++ scripts/plotEnsembleLandmarks.R
@@ -16,17 +16,17 @@ d_long <- d_long %>% group_by(landmark) %>% mutate(mean_x = mean(value.x),
 
 #Plot without procrustes
 (gg<- ggplot(d_long, aes()) + 
-  xlab("x") + ylab("y") +
-  xlim(180, 500) + ylim(45, 180) +
-  coord_fixed(ratio = 1, expand = TRUE, clip = "on") +
-  geom_point(x=d_long$value.x, y=d_long$value.y, fill="black", size=0.1) +
-  geom_point(x=d_long$mean_x, y=d_long$mean_y, color="red", size=2) + 
-  geom_errorbarh(aes(xmin=d_long$mean_x - d_long$sd_x,
-                     xmax=d_long$mean_x + d_long$sd_x,
-                     y=d_long$mean_y), colour="red", stat = "identity", size=1) +
-  geom_errorbar(aes(ymin=d_long$mean_y - d_long$sd_y,
-                    ymax=d_long$mean_y + d_long$sd_y,
-                    x=d_long$mean_x), colour="red", stat = "identity", size=1))
+    xlab("x") + ylab("y") +
+    xlim(180, 500) + ylim(45, 180) +
+    coord_fixed(ratio = 1, expand = TRUE, clip = "on") +
+    geom_point(x=d_long$value.x, y=d_long$value.y, fill="black", size=0.1) +
+    geom_point(x=d_long$mean_x, y=d_long$mean_y, color="red", size=2) + 
+    geom_errorbarh(aes(xmin=d_long$mean_x - d_long$sd_x,
+                       xmax=d_long$mean_x + d_long$sd_x,
+                       y=d_long$mean_y), colour="red", stat = "identity", size=1) +
+    geom_errorbar(aes(ymin=d_long$mean_y - d_long$sd_y,
+                      ymax=d_long$mean_y + d_long$sd_y,
+                      x=d_long$mean_x), colour="red", stat = "identity", size=1))
 
 ##With procrustes
 library(vegan)
@@ -48,11 +48,11 @@ doProcrustes<-function(target, d){
 ##Doesnt make much sense
 calc_vector1 <- function(tab){
   #Assumes variables in tibble are grouped
-   tab <- tab %>% mutate(x_vec = x - mean_x,
-                         y_vec = y - mean_y,
-                         mean_xvec = mean_x + mean(x_vec),
-                         mean_yvec = mean_y + mean(y_vec))
-   return(tab)
+  tab <- tab %>% mutate(x_vec = x - mean_x,
+                        y_vec = y - mean_y,
+                        mean_xvec = mean_x + mean(x_vec),
+                        mean_yvec = mean_y + mean(y_vec))
+  return(tab)
 }
 
 
@@ -83,10 +83,10 @@ pop_rotated <- doProcrustes(target, pop) %>% calc_vector1
     geom_errorbar(aes(ymin=mean_y - sd_y,
                       ymax=mean_y + sd_y,
                       x=mean_x), colour="red", stat = "identity") +
-  theme(element_blank()) + theme_bw()
-        )
+    theme(element_blank()) + theme_bw()
+)
 
-  (gg<- ggplot(pop_rotated , aes(x, y)) + 
+(gg<- ggplot(pop_rotated , aes(x, y)) + 
     geom_point(size=0.5, alpha=0.2) +
     xlab("x") + ylab("y") +
     coord_fixed(ratio = 1, expand = TRUE, clip = "on") +
@@ -123,35 +123,41 @@ pop_rotated <- doProcrustes(target, pop) %>% calc_vector1
 
 
 ### Plot arrows with PCA
-#without procrustes
-p <- d %>% select(matches("([xy][1234]|[xy]12)$", perl=T)) %>% as.matrix %>% cov
-#with procrustes
-simsexp <- sims_rotated %>% 
-  select(c(name, x, y, landmark)) %>% 
-  gather("coord", "value", x:y) %>% 
-  unite(temp, coord, landmark, sep="") %>% 
-  spread(key=temp, value=value ) 
-p <- simsexp %>%select(-name) %>% as.matrix %>% cov
-
-det_p <- det(p)
-v<-eigen(p)$vectors
-v1 <- v[, 1] %>% set_names(expand.grid(c("x", "y"), c("1", "2", "3", "4", "12")) %>% 
-                            as.matrix %>% apply(.,  MARGIN =1,FUN= paste, collapse="", sep=""))
-
-mmm <- data.frame()
-for(i in c(1, 2, 3, 4, 12)){
-  var1 <- simsexp %>% pull(paste("x", as.character(i), sep="", collapse=""))
-  var2 <- simsexp %>% pull(paste("y", as.character(i), sep="", collapse=""))  
-  aux <- data.frame(x0=mean(var1), 
-                    y0=mean(var2), 
-                    x1=mean(var1) + 0.1*v1[paste("x", as.character(i), sep="", collapse="")], 
-                    y1=mean(var2) + 0.1*v1[paste("y", as.character(i), sep="", collapse="")])
-  aux$landmark = i
-  mmm <- rbind(mmm, aux)
+getArrows <- function(data){
+  #without procrustes
+  #p <- d %>% select(matches("([xy][1234]|[xy]12)$", perl=T)) %>% as.matrix %>% cov
+  #with procrustes
+  simsexp <- data %>% 
+    select(c(name, x, y, landmark)) %>% 
+    gather("coord", "value", x:y) %>% 
+    unite(temp, coord, landmark, sep="") %>% 
+    spread(key=temp, value=value ) 
+  p <- simsexp %>%select(-name) %>% as.matrix %>% cov
   
-}
+  det_p <- det(p)
+  v<-eigen(p)$vectors
+  v1 <- v[, 1] %>% set_names( simsexp %>% select(matches("^x|y")) %>% names ) 
+  
+  mmm <- data.frame()
+  for(i in c(1, 2, 3, 4, 12)){
+    var1 <- simsexp %>% pull(paste("x", as.character(i), sep="", collapse=""))
+    var2 <- simsexp %>% pull(paste("y", as.character(i), sep="", collapse=""))  
+    aux <- data.frame(x0=mean(var1), 
+                      y0=mean(var2), 
+                      x1=mean(var1) + 0.1*v1[paste("x", as.character(i), sep="", collapse="")], 
+                      y1=mean(var2) + 0.1*v1[paste("y", as.character(i), sep="", collapse="")])
+    aux$landmark = i
+    mmm <- rbind(mmm, aux)
+  }
+  return(mmm) 
+}##getArrows()
 
-par(mfrow=c(1,1))
+mmm <- getArrows(pop_rotated)
+par(mfrow=c(1,2))
+plot(mmm$x0, mmm$y0, xlab = "x", ylab = "y", xlim = c(-0.3, 0.6), ylim=c(-0.3, 0.2))
+arrows(mmm$x0, mmm$y0, mmm$x1, mmm$y1, col = "red")
+for(i in 1:nrow(mmm)) segments(mmm$x0[i], mmm$y0[i], mmm$x0[(i)%%nrow(mmm)+1]  , mmm$y0[(i)%%nrow(mmm)+1]  )
+mmm <- getArrows(sims_rotated)
 plot(mmm$x0, mmm$y0, xlab = "x", ylab = "y", xlim = c(-0.3, 0.6), ylim=c(-0.3, 0.2))
 arrows(mmm$x0, mmm$y0, mmm$x1, mmm$y1, col = "red")
 for(i in 1:nrow(mmm)) segments(mmm$x0[i], mmm$y0[i], mmm$x0[(i)%%nrow(mmm)+1]  , mmm$y0[(i)%%nrow(mmm)+1]  )
